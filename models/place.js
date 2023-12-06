cce27fd3193aa0117b47b63e3433ed6f3ff34cce
Dan <dan.edmunds7@gmail.com>
2023-12-06 16:09:26 +0000

    Trying to fix seed

--- models/place.js
+++ models/place.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
 const placeSchema = new mongoose.Schema({
-    name: { type: String },
-    description: { type: String },
-    region: { type: String },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    region: { type: String, required: true },
     image: { type: String },
     occupiedBy: { type: [String] }
 }, {
