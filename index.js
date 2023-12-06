25862bed8a9d1e4724a7af1a4fe1012e91827551
Dan <dan.edmunds7@gmail.com>
2023-12-06 12:22:02 +0000

    On to models

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import 'dotenv/config'
+import router from './config/routes.js'
 
+const app = express()
+
+app.use(express.json())
+
+
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`)
+    next()
+})
+
+// Endpoints
+app.use('/api', router)
+
+async function startServer() {
+    try {
+        await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log('Database connection established')
+        app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`))
+    } catch (error) {
+        console.log('Error establishing connection')
+        console.log(error)
+    }
+}
+startServer()
