90cbea97f0f17f218c0018d8df61f74b8b636fad
Ben Kelly <benelliottkelly@gmail.com>
2023-12-06 13:40:12 +0000

    Added model for houses

---
+++ models/houses.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const housesSchema = mongoose.Schema({
+  houseName: {type: String, required: true, unique: true, maxlength: 60},
+  crest: {type: String, required: true},
+  description: {type: String, required: true},
+  notableBattles: [{type: String}],
+  bannermen: [{type: String}],
+})
+
+housesSchema
+	.virtual('characters', {
+		ref: 'Character',
+		localField: 'houseName',
+		foreignField: 'house'
+})
+
+housesSchema
+  .virtual('places', {
+    ref: 'Place',
+    localField: 'houseName',
+    foreignField: 'occupiedBy'
+})
+
+export default mongoose.model('House', housesSchema)
