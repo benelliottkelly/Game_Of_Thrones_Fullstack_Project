843a9ec7a4ac3a61fca8a0d8bb45c496ce9f15ee
Ben Kelly <benelliottkelly@gmail.com>
2023-12-06 15:54:52 +0000

    added controllers and routes for houses

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,16 @@
 import express from 'express'
 
+import {
+  getAllHouses,
+  getSingleHouse
+} from '../controllers/houses.js'
+
 const router = express.Router()
+
+router.route('/houses')
+  .get(getAllHouses)
+
+router.route('/houses/houseId')
+  .get(getSingleHouse)
 
 export default router
---
+++ controllers/houses.js
@@ -0,0 +1,20 @@
+import House from '../models/house.js'
+
+export const getAllHouses = async (req, res) => {
+  const houses = await House.find()
+  return res.status(200).json(houses)
+}
+
+export const getSingleHouse = async (req, res) => {
+  try {
+    const { houseId } = req.params
+    const house = await House.findById(houseId)
+    if (!house) {
+      return res.ststus(404).json({ message: 'There is no house of this name.'})
+    }
+    return res.json(house)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(error)
+  }
+}
