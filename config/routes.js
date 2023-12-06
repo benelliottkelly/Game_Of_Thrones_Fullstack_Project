43e97b30b1d2bac1e28747a5ad1f12de3575f46b
NyashaDZT <nyashapatrickdzvoti@live.co.uk>
2023-12-06 15:47:40 +0000

    character methods added

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import { createCharacter, deleteCharacter, getAllCharacters, showSingleCharacter, updateCharacter } from '../controllers/characters'
 
 const router = express.Router()
+
+// characters
+router.route('/characters')
+  .get(getAllCharacters)
+  .post(createCharacter)
+
+router.route('/characters/:characterId')
+  .get(showSingleCharacter)
+  .put(updateCharacter)
+  .delete(deleteCharacter)
 
 export default router
