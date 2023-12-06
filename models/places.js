4bc6ef7356160a6f78fca7bf8355320d9200d5d5
Dan <dan.edmunds7@gmail.com>
2023-12-06 13:27:37 +0000

    Places Model created

---
+++ models/places.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    region: { type: String, required: true },
+    image: { type: String },
+    occupiedBy: { type: [String] }
+})
+
+placeSchema.set('toJSON', {
+    virtuals: true
+})
+
+placeSchema.virtual('charactersInPlace', {
+    ref: 'Character',
+    localField: 'name',
+    foreignField: 'hometown'
+})
+
+export default mongoose.model('Place', placeSchema)
