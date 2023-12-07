0aa6b7c56868d43d875f56805a7961798c5a72ec
Dan <dan.edmunds7@gmail.com>
2023-12-07 11:37:01 +0000

    Index started

--- client/src/components/AllPlaces.jsx
+++ client/src/components/AllPlaces.jsx
@@ -19,6 +19,19 @@ export default function AllPlaces() {
     return (
         <>
             <h1>All Places</h1>
+            {places.map(place => {
+                return (
+                    <>
+                        <div className="card" key={place.id}>
+                            <div className="card-body">
+                                <h5 className="card-title">{place.name}</h5>
+                                <p className="card-text">{place.description}</p>
+                            </div>
+                        </div>
+                    </>
+                )
+            })
+            }
         </>
     )
 }
--- client/src/utils/loaders.js
+++ client/src/utils/loaders.js
@@ -1,31 +1,29 @@
-import axios from "axios"
-
 export async function characterLoader() {
-    const { data } = await axios.get(`/api/characters`)
-    return data.json()
+    const res = await fetch(`/api/characters`)
+    return res.json()
 }
 
 export async function houseLoader() {
-    const { data } = await axios.get(`/api/houses`)
-    return data.json()
+    const res = await fetch(`/api/houses`)
+    return res.json()
 }
 
 export async function placeLoader() {
-    const { data } = await axios.get(`/api/places`)
-    return data.json()
+    const res = await fetch(`/api/places`)
+    return res.json()
 }
 
 export async function singleCharacterLoader(characterId) {
-    const { data } = await axios.get(`/api/characters/${characterId}`)
-    return data.json()
+   const res = await fetch(`/api/characters/${characterId}`)
+    return res.json()
 }
 
 export async function singleHouseLoader(houseId) {
-    const { data } = await axios.get(`/api/houses/${houseId}`)
-    return data.json()
+   const res = await fetch(`/api/houses/${houseId}`)
+    return res.json()
 }
 
 export async function singlePlaceLoader(placeId) {
-    const { data } = await axios.get(`/api/places/${placeId}`)
-    return data.json()
+   const res = await fetch(`/api/places/${placeId}`)
+    return res.json()
 }
