9e6535927fd5251d68a27e51a7de60ff162e2d9b
Dan <dan.edmunds7@gmail.com>
2023-12-07 10:46:55 +0000

    Fixed small issue

--- client/src/utils/loaders.js
+++ client/src/utils/loaders.js
@@ -2,30 +2,30 @@ import axios from "axios"
 
 export async function characterLoader() {
     const { data } = await axios.get(`/api/characters`)
-    return { data }.json()
+    return data.json()
 }
 
 export async function houseLoader() {
     const { data } = await axios.get(`/api/houses`)
-    return { data }.json()
+    return data.json()
 }
 
 export async function placeLoader() {
     const { data } = await axios.get(`/api/places`)
-    return { data }.json()
+    return data.json()
 }
 
 export async function singleCharacterLoader(characterId) {
     const { data } = await axios.get(`/api/characters/${characterId}`)
-    return { data }.json()
+    return data.json()
 }
 
 export async function singleHouseLoader(houseId) {
     const { data } = await axios.get(`/api/houses/${houseId}`)
-    return { data }.json()
+    return data.json()
 }
 
 export async function singlePlaceLoader(placeId) {
     const { data } = await axios.get(`/api/places/${placeId}`)
-    return { data }.json()
+    return data.json()
 }
