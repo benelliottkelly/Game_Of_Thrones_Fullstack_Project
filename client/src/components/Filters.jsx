215251f502291c8acc13a6a4a703291095960a63
Dan <dan.edmunds7@gmail.com>
2023-12-07 23:31:02 +0000

    struggling to make houses searchbar work

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,14 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react"
 
-
-
 export default function Filter({ places, setFilteredPlaces, setfilteredHouses, houses, characters, setFilteredCharacters }) {
 
     const [filters, setFilters] = useState({
         search: ''
     })
-
 
     function handleSearch(e) {
         const newObj = {
@@ -18,36 +15,34 @@ export default function Filter({ places, setFilteredPlaces, setfilteredHouses, h
         setFilters(newObj)
     }
 
-
     useEffect(() => {
         const pattern = new RegExp(filters.search, 'i')
         let filteredArray
         if (places) {
             filteredArray = places.filter(place => {
+                console.log(place.occupiedBy)
                 return pattern.test(place.name) || pattern.test(place.occupiedBy)
             })
             setFilteredPlaces(filteredArray)
 
 
         } else if (houses) {
-            filteredArray = houses.filter(house => {
-                if (house.characters) {
-            
-                    const filteredChars = house.characters.filter(char => {
-                        return pattern.test(char.firstName) || 
-                        pattern.test(char.lastName)
-                    })
-                    if (filteredChars.length > 0) {
-                        house.characters = filteredChars
-                        return true
+            filteredArray = houses.filter(abode => {
+                    if (abode.characters.length === 0) {
+                        return pattern.test(abode.houseName) 
+                    } else if (abode.characters.length > 0) {
+                        let filtArr = abode.characters.filter(char => {
+                            console.log(char.firstName)
+                           return char === 0
+                        })
+                        
+                        console.log(filtArr)
+                        
+                        return pattern.test(abode.houseName) 
+                        || pattern.test(filtArr)
+
                     }
-                }
-                // if (house.occupiedBy) {
-                //     const filteredPlaces = house.occupiedBy.filter(place => {
-                //         console.log(place)
-                //     })
-                // }
-                return pattern.test(house.houseName)
+            
             })
             setfilteredHouses(filteredArray)
 
@@ -55,6 +50,7 @@ export default function Filter({ places, setFilteredPlaces, setfilteredHouses, h
         } else if (characters) {
             filteredArray = characters.filter(char => {
                 if (char.associatedHouse[0]) {
+                    console.log(char.associatedHouse[0].houseName)
                     return pattern.test(char.firstName) ||
                         pattern.test(char.lastName) ||
                         pattern.test(char.associatedHouse[0].houseName)
@@ -63,21 +59,6 @@ export default function Filter({ places, setFilteredPlaces, setfilteredHouses, h
             setFilteredCharacters(filteredArray)
         }
     }, [filters, places, setFilteredPlaces, houses, setfilteredHouses, characters, setFilteredCharacters])
-
-    // useEffect(() => {
-    //     const pattern2 = new RegExp(filters.search, 'i')
-    //     const filteredHouseArray = houses.filter(house => {
-    //         return pattern2.test(house.houseName)
-    //     })
-    //     setfilteredHouses(filteredHouseArray)
-    // }, [filters, houses, setfilteredHouses])
-
-
-    // const houseObj = {
-    //     ...filters,
-    //     [e.target.houseName]: e.target.value
-    // }
-    // setFilters(houseObj)
 
     return (
         <>
