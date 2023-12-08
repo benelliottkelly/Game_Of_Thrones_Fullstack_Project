78d2ef2da7071ad8098e890548773bd2416352bd
NyashaDZT <nyashapatrickdzvoti@live.co.uk>
2023-12-08 15:23:31 +0000

    friday

--- client/src/components/CreateCharacter.jsx
+++ client/src/components/CreateCharacter.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Form, useActionData, useNavigate } from 'react-router-dom'
+import ImageUploadField from "./ImageUploadField"
 
 export default function CreateCharacter(){
 
@@ -12,8 +13,12 @@ export default function CreateCharacter(){
     image: '',
     battles: [],
     biography: '',
-    house: ''
+    hometown: ''
     })
+
+    function handleChange(e){
+      setFormData ({ ...formData, [e.target.name]: e.target.value })
+    }
 
   useEffect(() => {
     console.log(res)
@@ -27,6 +32,12 @@ export default function CreateCharacter(){
     <>
       <h1 className="text-center bold display-3 mb-4">Create A Character</h1>
       <Form className="form" method="POST">
+        <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} />
+        <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName}  />
+        <ImageUploadField setFormData={setFormData} formData={formData} />
+        <input type="text"  name="biography" placeholder="Bio" value={formData.biography}  />
+        <input type="text" name="hometown" placeholder="Hometown"  value={formData.hometown} />
+        <input type="submit" value="Create Character" />
       </Form>
     </>
   )
