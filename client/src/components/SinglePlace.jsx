c9d112cfbdf31086299ac3a10a2fe22114d2b919
Ben Kelly <benelliottkelly@gmail.com>
2023-12-12 16:34:33 +0000

    added styling to newly added houses

--- client/src/components/SinglePlace.jsx
+++ client/src/components/SinglePlace.jsx
@@ -10,15 +10,13 @@ export default function SinglePlace() {
 
   const loadedData = useLoaderData()
   console.log(loadedData)
-  const { charactersInPlace, description, image, name, occupyingHouses, region } = loadedData
-
+  const { charactersInPlace, description, image, name, occupiedBy, occupyingHouses, region } = loadedData
   function scrollUp() {
     document.documentElement.scrollTop = 0
   }
-  const { houseName } = occupyingHouses[0]
 
   return (
-    <Container fluid className={`${houseName}-container`}>
+    <Container fluid className={`${occupyingHouses.length > 0 ? occupyingHouses[0].houseName : "general"}-container`}>
       <Link className="index-return" to={`/places`}><GiCrossedSwords /></Link>
       <Row xs={12} md={12} lg={12}>
         <Col>
@@ -61,15 +59,15 @@ export default function SinglePlace() {
         <Row className="p-5" xs={12} md={12} lg={12}>
           <Col>
             {charactersInPlace.length > 0 &&
-              <div className="character-text">
+              <div className="stained-glass">
                 <h3>Related Characters</h3>
-                <div className="charactersInPlace-container">
+                <div className="house-member-container">
                   {charactersInPlace.map((character, idx) => {
-                    return <Link onClick={scrollUp} className="character" key={idx} to={`/characters/${character.id}`}>
+                    return <Link onClick={scrollUp} className="house-member" key={idx} to={`/characters/${character.id}`}>
                       <div>
-                        <div className="individual-charactersInPlace">
+                        <div className="individual">
                           <h2>{`${character.firstName} ${character.lastName}`}</h2>
-                          <img className='character-picture' src={character.image} alt={`Image of ${character.firstName} ${character.lastName}`} />
+                          <img className='individual-picture' src={character.image} alt={`Image of ${character.firstName} ${character.lastName}`} />
                         </div>
                       </div>
                     </Link>
