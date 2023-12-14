f656ef5d56317f87d30b877c68a98856c2e8db47
Ben Kelly <benelliottkelly@gmail.com>
2023-12-14 13:35:40 +0000

    changed the hover animation for links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 // Components
 import Nav from './components/Nav'
 import NavbarFunction from './components/Nav'
 
+
+
 function App() {
+  const navigation = useNavigation()
+  console.log(navigation.state)
   return (
     <>
       <NavbarFunction />
       <main>
-        <Outlet />
+        {
+          navigation.state === "idle" ?
+            <Outlet />
+            :
+            <article className='loading-container'>
+              <div className='loading-screen'></div>
+            </article>
+        }
       </main>
     </>
   )
